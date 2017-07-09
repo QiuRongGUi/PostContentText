7b964c25a2cc33179fe68a34c49ec08921196209
QIUGUI <15201314165@163.com>
2017-07-10 00:29:13 +0800

    初始化 Demo

---
+++ PostContentText/PostContentText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PostContentText
+//
+//  Created by QIUGUI on 2017/7/10.
+//  Copyright © 2017年 QIUGUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
